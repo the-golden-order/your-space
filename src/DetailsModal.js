f827ccd0c353b65bc191efe5a92239cbbde12489
Roger Wells <rsw359@gmail.com>
2022-04-05 08:56:04 +0900

    props to profile

--- src/DetailsModal.js
+++ src/DetailsModal.js
@@ -2,5 +2,15 @@ import React from "react";
 import { Modal, Button } from "react-bootstrap";
 
 class DetailsModal extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state= {
+      artistName: '',
+      trackName:'',
+      artWork: '',
+      genre: '',
+      note:'',
+    }
+  };
   
 }
