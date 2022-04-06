7f3d8c754cc0827bd4fde9f4fbd5b1b6ed612b42
Roger Wells <rsw359@gmail.com>
2022-04-07 08:22:03 +0900

    empty state tests

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import React from 'react';
 import Header from './Header';
 // // import Main from './Main';
 import Footer from './Footer';
-import BestMusic from './YourspaceMain';
+import Main from './Main';
 import Login from './Login';
-import Profile from './Profile'
+import UserProfile from './Profile'
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {
@@ -12,10 +12,10 @@ import {
   Switch,
   Route
 } from "react-router-dom";
-import OurTeam from "./OurTeam"
 import { withAuth0 } from '@auth0/auth0-react';
+import axios from 'axios';
 
-
+let SERVER = process.env.REACT_APP_SERVER;
 
 class App extends React.Component {
 
@@ -23,9 +23,118 @@ class App extends React.Component {
     super(props);
     this.state = {
       user: '',
-      email: ''
+      email: '',
+      music: [],
+      itunesAPI: [],
+      query: '',
+      dbSongs:[],
+      showModal: false,
     }
   }
+
+  handleMusicInput = (e) => {
+    this.setState({
+      query: e,
+    });
+  }
+
+  getItunesData = async () => {
+    // e.preventDefault();
+    try {
+
+      let itunesData = await axios.get(`${SERVER}/itunes?term=${this.state.query}`);//TODO term= this.state.query
+
+      this.setState({
+        itunesAPI: itunesData.data
+      });
+    } catch (error) {
+      console.log('error updating', error.message);
+    }
+  };
+
+  getMusic = async () => {
+    // JSON Web Token = JWT (pronounced JOT)
+    if (this.props.auth0.isAuthenticated) {
+      // get token:
+      const res = await this.props.auth0.getIdTokenClaims();
+
+      const jwt = res.__raw;
+      console.log(jwt);
+      // config object to make our call 
+      const config = {
+        method: 'get',
+        baseURL: process.env.REACT_APP_SERVER,
+        url: '/music',
+        headers: { "Authorization": `Bearer ${jwt}` }
+      };
+      const musicResults = await axios(config);
+
+      this.setState({
+        music: musicResults.data
+      })      
+    }
+    
+  }
+
+  componentDidMount(){
+    this.getMusic();
+    console.log(this.state.music);
+  }
+
+  postMusic = async (newMusic) => {
+    try {
+      let results = await axios.post(`${SERVER}/music`, newMusic);
+      this.setState({
+        music: [...this.state.music, results.data]
+      });
+    } catch (error) {
+      console.error('error', error.response);
+    }
+  }
+
+  updateMusic = async (updatedEntry) => {
+    try {
+      let url = `${SERVER}/music/${updatedEntry._id}`;
+      let updatedMusic = await axios.put(url, updatedEntry);
+      console.log(updatedEntry);
+      let updatedMusicData = this.state.music.map(currentMusic => {
+        return currentMusic._id === updatedEntry._id ?
+          updatedMusic.data :
+          currentMusic;
+      });
+      this.setState({
+        music: updatedMusicData
+      });
+    } catch (error) {
+      console.log('error updating', error.message);
+    }
+  };
+
+  deleteMusic = async (id) => {
+    try {
+      let url = `${SERVER}/music/${id}`;
+      await axios.delete(url);
+      let updatedMusic = this.state.music.filter(Music => Music._id !== id);
+      this.setState({
+        music: updatedMusic
+      })
+    } catch (error) {
+      console.log('error, doggy', error.response.data);
+    }
+    this.getMusic();
+  };
+
+  displayModal = () => {
+    this.setState({
+      showModal: true,
+    });
+  };
+
+  hideModal = () => {
+    this.setState({
+      showModal: false
+    });
+  };
 
   logInHandler = (user) => {
     console.log(user);
@@ -56,6 +165,7 @@ class App extends React.Component {
 //    )
 //  }
   render() {
+    console.log(this.state.music)
     return (
       <>
         <Router>
@@ -67,7 +177,23 @@ class App extends React.Component {
             <Route exact path="/">
               {this.props.auth0.isAuthenticated
                 ?
-                <BestMusic email={this.state.email} />  // NEEED TOOOO UUUUUPPPPDDDAAATTEEE
+                <Main
+                user={this.state.user}
+                email={this.state.email} 
+                music={this.state.music}
+                itunesAPI={this.state.itunesAPI}
+                query={this.state.query}
+                dbSongs={this.state.dbSongs}
+                showModal={this.state.showModal}
+
+                getItunesData={this.getItunesData} 
+                handleMusicInput={this.handleMusicInput}
+                postMusic={this.postMusic}
+                updateMusic={this.updateMusic}
+                deleteMusic={this.deleteMusic}
+                displayModal={this.displayModal}
+                hideModal={this.hideModal}
+                />  // NEEED TOOOO UUUUUPPPPDDDAAATTEEE
                 :
                 <Login
                   userHandler={this.userHandler}
@@ -75,19 +201,32 @@ class App extends React.Component {
                 />
               }
             </Route>
-            <Route exact path="/Profile">
-              {this.props.auth0.isAuthenticated
+            <Route exact path="/UserProfile">
+              {this.props.auth0.isAuthenticateds
                 ?
-                <Profile
+                <UserProfile
                   user={this.state.user}
                   email={this.state.email}
+                  music={this.state.music}
+
+
+                  itunesAPI={this.state.itunesAPI}
+                  query={this.state.query}
+                  dbSongs={this.state.dbSongs}
+                  showModal={this.state.showModal}
+  
+                  getItunesData={this.getItunesData} 
+                  handleMusicInput={this.handleMusicInput}
+                  postMusic={this.postMusic}
+                  updateMusic={this.updateMusic}
+                  deleteMusic={this.deleteMusic}
+                  displayModal={this.displayModal}
+                  hideModal={this.hideModal}
+  
                 />
                 :
                 null
               }
-            </Route>
-            <Route exact path="/OurTeam">
-              <OurTeam/>
             </Route>
           </Switch>
           <Footer />
---
+++ src/Main.js
@@ -0,0 +1,194 @@
+// import axios from "axios";
+import React from "react";
+import { Container } from "react-bootstrap";
+import { withAuth0 } from "@auth0/auth0-react";
+// import Profile from "./Profile";
+import SearchBar from "./SearchBar";
+import MainCard from './MainCard'
+import OurTeam from "./OurTeam";
+import './Button.css'
+import UserProfile from "./UserProfile";
+// import Footer from './Footer'
+// import Header from './Header'
+// let SERVER = process.env.REACT_APP_SERVER;
+
+class Main extends React.Component {
+
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     music: [],
+  //     itunesAPI: [],
+  //     query: '',
+  //     dbSongs:[],
+  //     showModal: false,
+  //     auth0Email: this.props.auth0.user.email
+  //   }
+  // }
+
+  // handleMusicInput = (e) => {
+  //   this.setState({
+  //     query: e,
+  //   });
+  // }
+
+  // getItunesData = async () => {
+  //   // e.preventDefault();
+  //   try {
+
+  //     let itunesData = await axios.get(`${SERVER}/itunes?term=${this.state.query}`);//TODO term= this.state.query
+
+  //     this.setState({
+  //       itunesAPI: itunesData.data
+  //     });
+  //   } catch (error) {
+  //     console.log('error updating', error.message);
+  //   }
+  // };
+
+//  handleMusicInput = (event) => {
+//    this.setState({
+//      query: event.target.value,
+//    });
+//  }
+
+
+  // getMusic = async () => {
+  //   // JSON Web Token = JWT (pronounced JOT)
+  //   if (this.props.auth0.isAuthenticated) {
+  //     // get token:
+  //     const res = await this.props.auth0.getIdTokenClaims();
+
+  //     const jwt = res.__raw;
+  //     console.log(jwt);
+  //     // config object to make our call 
+  //     const config = {
+  //       method: 'get',
+  //       baseURL: process.env.REACT_APP_SERVER,
+  //       url: '/music',
+  //       headers: { "Authorization": `Bearer ${jwt}` }
+  //     };
+  //     const musicResults = await axios(config);
+
+  //     console.log(musicResults.data);
+  //     this.setState({
+  //       music: musicResults.data
+  //     })
+      
+  //   }
+  // }
+  // componentDidMount() {
+  //   this.getMusic();
+  //   console.log(this.state.music);
+  // }
+
+  // postMusic = async (newMusic) => {
+  //   try {
+  //     let results = await axios.post(`${SERVER}/music`, newMusic);
+  //     this.setState({
+  //       music: [...this.state.music, results.data]
+  //     });
+  //   } catch (error) {
+  //     console.error('error', error.response);
+  //   }
+  // }
+
+  // updateMusic = async (updatedEntry) => {
+  //   try {
+  //     let url = `${SERVER}/music/${updatedEntry._id}`;
+  //     let updatedMusic = await axios.put(url, updatedEntry);
+  //     console.log(updatedEntry);
+  //     let updatedMusicData = this.state.music.map(currentMusic => {
+  //       return currentMusic._id === updatedEntry._id ?
+  //         updatedMusic.data :
+  //         currentMusic;
+  //     });
+  //     this.setState({
+  //       x: updatedMusicData
+  //     });
+  //   } catch (error) {
+  //     console.log('error updating', error.message);
+  //   }
+  // };
+
+  // deleteMusic = async (id) => {
+  //   try {
+  //     let url = `${SERVER}/music/${id}`;
+  //     await axios.delete(url);
+  //     let updatedMusic = this.state.music.filter(Music => Music._id !== id);
+  //     this.setState({
+  //       music: updatedMusic
+  //     })
+  //   } catch (error) {
+  //     console.log('error, doggy', error.response.data);
+  //   }
+  //   this.getMusic();
+  // };
+
+
+
+  // displayModal = () => {
+  //   this.setState({
+  //     showModal: true,
+  //   });
+  // };
+
+  // hideModal = () => {
+  //   this.setState({
+  //     showModal: false
+  //   });
+  // };
+
+
+
+  render() {
+    // console.log(this.props.auth0.user);
+    // console.log(this.props.auth0.user.email);
+    console.log(this.props.music);
+    let allResults = this.props.itunesAPI.map((query, index) => {
+      return (
+        <MainCard
+          key={index}
+          query={query} 
+          postMusic={this.props.postMusic}
+          auth0Email={this.props.auth0.user.email}/>
+      );
+    })
+
+
+    // let addedSongs = this.state.music.map((dbSongs, idx) => {
+    //   return ( 
+    //   // <Container key={idx}>
+
+    //   // </Container>
+    //   <ul key={idx}>
+    //     <li>{dbSongs.artistName}</li>
+    //   </ul>
+
+
+
+    //   );
+    // })
+    console.log('Query: ', this.props.query);
+    return (
+      <>
+        <Container>
+          <SearchBar getItunesData={this.props.getItunesData} handleMusicInput={this.props.handleMusicInput} />  
+        </Container>
+
+        <Container>
+          <main>
+            {allResults}            
+          </main>
+        </Container>
+
+        <UserProfile music={this.props.music}/>
+        
+       <OurTeam/>
+      </>
+    )
+
+  }
+}
+
+export default withAuth0(Main)
